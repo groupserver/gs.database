9c287b3938d5358461c1f9f685fa9fa1c85c1ae7
Michael JasonSmith <mpj17@onlinegroups.net>
2013-08-09 10:18:38 -0700

    Updating the license and copyright.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -11,32 +11,31 @@ setup(name='gs.database',
     long_description=open("README.txt").read() + "\n" +
                       open(os.path.join("docs", "HISTORY.txt")).read(),
     classifiers=[
-      "Development Status :: 4 - Beta",
-      "Environment :: Web Environment",
-      "Framework :: Zope2",
-      "Intended Audience :: Developers",
-      "License :: Other/Proprietary License",
-      "Natural Language :: English",
-      "Operating System :: POSIX :: Linux"
-      "Programming Language :: Python",
-      "Topic :: Software Development :: Libraries :: Python Modules",
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Zope Public License',
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
       ],
     keywords='groupserver option options',
     author='Richard Waid',
     author_email='richard@onlinegroups.net',
     url='http://groupserver.org/',
-    license='other',
+    license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs'],
     include_package_data=True,
     zip_safe=True,
     install_requires=[
-        'setuptools',
-        'gs.config[zope]',  # Note: With Zope support
-        'sqlalchemy',
         'psycopg2',
+        'setuptools',
+        'sqlalchemy',
         'zope.sqlalchemy',
-        # -*- Extra requirements: -*-
+        'gs.config[zope]',  # Note: With Zope support
     ],
     entry_points="""
     # -*- Entry points: -*-
