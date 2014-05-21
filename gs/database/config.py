b062f0ab30871848fe63a461afd99c44faa5494d
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-21 09:32:20 -0700

    Switching to Unicode literals and absolute import

--- gs/database/config.py
+++ gs/database/config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Copyright © 2014 OnlineGroups.net and Contributors.
+# Copyright © 2012, 2013, 2014 OnlineGroups.net and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -12,14 +12,14 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-import time
+from __future__ import absolute_import, unicode_literals
+from logging import getLogger
+log = getLogger('gs.database')
+from time import time
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import create_engine, ThreadLocalMetaData
 from zope.sqlalchemy import ZopeTransactionExtension
 from gs.config import Config, getInstanceId
-
-import logging
-log = logging.getLogger('gs.database')
 
 databases = {}
 
@@ -46,9 +46,9 @@ def get_db(instance_id=None):
         config.set_schema('database', {'dsn': str})
         dsn = config.get('database')['dsn']
 
-        top = time.time()
+        top = time()
         databases[instance_id] = init_db(dsn)
-        bottom = time.time()
+        bottom = time()
         log.info("Initialised database for instance %s in %.2fs" %
                  (instance_id, (bottom - top)))
     return databases[instance_id]
